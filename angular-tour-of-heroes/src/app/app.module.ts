fea3cb4fd0cc5ac4fc243f4443823c8e268ffc2d
Duggal, Rajesh <Rajesh.Duggal@td.com>
2018-11-21 14:58:07 -0500

    Exec ng commands using angular-cli-7.0.5

---
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
